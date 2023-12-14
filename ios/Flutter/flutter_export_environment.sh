02d97a2208b5178c1f2cf99198df4cc6ab4b434d
Vishnu Agarwal <vishnuagbly@gmail.com>
2023-12-14 19:00:49 +0530

    Generated Files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -3,6 +3,7 @@
 export "FLUTTER_ROOT=/Users/vishnu/Desktop/Packages/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/vishnu/StudioProjects/advanced_polygons"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=2"
